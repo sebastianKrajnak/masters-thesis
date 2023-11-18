6a549e25f3a8421362bfb9dea400739d7348e704
Sebastian Krajnak <sebastian.krajnak@gmail.com>
2023-11-18 17:35:04 +0000

    setting up to work on server

--- Q-sort-App-main/q-sort-app/src/socket.js
+++ Q-sort-App-main/q-sort-app/src/socket.js
@@ -7,7 +7,8 @@ export const state = reactive({
 
 // "undefined" means the URL will be computed from the `window.location` object
 const server_URL =
-  process.env.NODE_ENV === "production" ? undefined : "http://localhost:3000";
+  process.env.NODE_ENV === "production" ? undefined : "http://ws-server:3000";
+console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
 
 const socket = io(server_URL);
 
