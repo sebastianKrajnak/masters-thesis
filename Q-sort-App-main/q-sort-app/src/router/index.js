4b36a05a4fdf2d9f29bf31627738ad61e7016d15
sebastianKrajnak <sebastian.krajnak@gmail.com>
2023-11-18 15:42:03 +0100

    done with dockerizing

--- Q-sort-App-main/q-sort-app/src/router/index.js
+++ Q-sort-App-main/q-sort-app/src/router/index.js
@@ -2,33 +2,32 @@
  * Routes for the application
  * @author Michal Janů
  */
-import { createRouter, createWebHistory } from "vue-router"
-import Sorting from "../views/Sorting.vue"
-import Home from "../views/Home.vue"
-import Ferewell from "../views/Ferewell.vue"
+import { createRouter, createWebHistory } from "vue-router";
+import Sorting from "../views/Sorting.vue";
+import Home from "../views/Home.vue";
+import Ferewell from "../views/Ferewell.vue";
 
 const routes = [
-	{
-		path: "/",
-		name: "Home",
-		component: Home
-	},
-	{
-		path: "/Sorting/:uid",
-		name: "Sorting",
-		component: Sorting
-	},
-	{
-		path: "/Final",
-		name: "Ferewell",
-		component: Ferewell
-	}
-
-]
+  {
+    path: "/xkrajn05",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/xkrajn05/Sorting/:uid",
+    name: "Sorting",
+    component: Sorting,
+  },
+  {
+    path: "/xkrajn05/Final",
+    name: "Ferewell",
+    component: Ferewell,
+  },
+];
 
 const router = createRouter({
-	history: createWebHistory(),
-	routes
-})
+  history: createWebHistory(),
+  routes,
+});
 
-export default router
+export default router;
--- Q-sort-App-main/q-sort-app/vite.config.js
+++ Q-sort-App-main/q-sort-app/vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from "vite"
-import vue from "@vitejs/plugin-vue"
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-	base: "/",
-	plugins: [vue()],
-	server: {
-		watch:{
-			usePolling: true
-		}
-	}
-})
+  base: "/xkrajn05/",
+  plugins: [vue()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
+});
