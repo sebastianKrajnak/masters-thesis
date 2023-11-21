496fb5eb33a4e06044e1b8abc24b93838f8b79cc
Sebastian Krajnak <sebastian.krajnak@gmail.com>
2023-11-21 13:35:59 +0000

    testing ws HMR but doesnt work, docker restructure

--- Q-sort-App-main/q-sort-app/vite.config.js
+++ Q-sort-App-main/q-sort-app/vite.config.js
@@ -5,14 +5,15 @@ export default defineConfig({
   base: "/xkrajn05/",
   plugins: [vue()],
   server: {
-    host: true,
+    host: 'localhost',
+    port: 8080,
     watch: {
       usePolling: true,
     },
-    hmr: {
-      host: "0.0.0.0",
-      port: 8080,
-    },
+    // hmr: {
+    //   host: "localhost",
+    //   port: 8080,
+    // },
     proxy : {
       '/api' : 'http://localhost:8080'
     }
